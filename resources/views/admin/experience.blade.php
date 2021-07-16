04fd864380e609347439ff864cc7bcd148752973
Your Name <you@example.com>
2021-07-16 12:50:19 +0700

    hanhutai

--- resources/views/admin/experience.blade.php
+++ resources/views/admin/experience.blade.php
@@ -4,24 +4,42 @@
         @csrf
         eventName:<br>
         <input type="text" name="eventName">
+        @error('eventName')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
         <br>
         bandNames:<br>
         <input type="text" name="bandNames">
+        @error('bandNames')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
         <br>
         startDate:<br>
         <input type="text" name="startDate">
+        @error('startDate')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
         <br>
         endDate:<br>
         <input type="text" name="endDate">
+        @error('endDate')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
         <br>
         portfolio:<br>
         <input type="text" name="portfolio">
         <br>
         ticketPrice:<br>
         <input type="text" name="ticketPrice">
+        @error('ticketPrice')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
         <br>
         status:<br>
         <input type="text" name="status">
+        @error('status')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
     </form>
 @endsection
 
